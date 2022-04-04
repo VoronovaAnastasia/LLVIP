bf819c7e1810ef2907cd74348be7f1d6a44c1b07
Anastasia Voronova <nastenckavoronowa@yandex.ru>
2022-04-05 00:10:35 +0300

    png images in plots

--- yolov5/utils/plots.py
+++ yolov5/utils/plots.py
@@ -148,7 +148,7 @@ def output_to_target(output):
     return np.array(targets)
 
 
-def plot_images(images, targets, paths=None, fname='images.jpg', names=None, max_size=1920, max_subplots=16):
+def plot_images(images, targets, paths=None, fname='images.png', names=None, max_size=1920, max_subplots=16):
     # Plot image grid with labels
     if isinstance(images, torch.Tensor):
         images = images.cpu().float().numpy()
