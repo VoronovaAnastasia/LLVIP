2a21f09dd1b7a6c8a0ba07486a8960c87e9bafd9
Anastasia Voronova <nastenckavoronowa@yandex.ru>
2022-04-10 18:40:14 +0300

    png in plot_images

--- yolov5/val.py
+++ yolov5/val.py
@@ -225,7 +225,7 @@ def run(data,
 
         # Plot images
         if plots and batch_i < 3:
-            f = save_dir / f'val_batch{batch_i}_labels.jpg'  # labels
+            f = save_dir / f'val_batch{batch_i}_labels.png'  # labels
             Thread(target=plot_images, args=(img, targets, paths, f, names), daemon=True).start()
             f = save_dir / f'val_batch{batch_i}_pred.png'  # predictions
             Thread(target=plot_images, args=(img, output_to_target(out), paths, f, names), daemon=True).start()
